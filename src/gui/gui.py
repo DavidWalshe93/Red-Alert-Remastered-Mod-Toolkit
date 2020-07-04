e11950ad461a0d8c792b8bb16362c7537bc6ec3c
DavidWalshe93 <david.walshe93@gmail.com>
2020-07-04 20:14:52 +0100

    cleaned up gui.py

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -4,15 +4,10 @@ Date:       29 June 2020
 """
 
 import logging
-
-import src.logger.logger_setup
-
-logger = logging.getLogger(__name__)
-
 import sys
 
-
 from PyQt5 import QtWidgets
+
 from src.db_driver.db_manager import DBManager
 from src.gui.controllers.unit.core import UnitCoreController
 from src.gui.view.app import MainWindow
@@ -24,15 +19,10 @@ def run():
 
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-
     window.show()
 
-    # Initial populate
-    db = DBManager()
+    # Setup controllers with view.
     unit_controller = UnitCoreController(window)
     unit_controller.bind_slots()
+
     app.exec_()
-
-
-
-
