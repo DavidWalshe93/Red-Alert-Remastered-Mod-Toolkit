f6ba7a0ad9446e7adca3bb63b7c8b4e7c54f73f7
DavidWalshe93 <david.walshe93@gmail.com>
2020-07-06 17:11:29 +0100

    Created central model code in db_drivers

--- src/db_driver/models/units/core.py
+++ src/db_driver/models/units/core.py
@@ -5,14 +5,11 @@ Date:       28 June 2020
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 
-from src.db_driver.connection_manager import ConnectionManager
-
-Base = ConnectionManager.base()
+from src.db_driver.models.model import Model, Base
 
 
-class Core:
+class Core(Model):
 
     Id = Column(Integer, primary_key=True)
     Name = Column(String, unique=True)
@@ -38,51 +35,6 @@ class Core:
     TechLevel = Column(Integer, default=-1)
     Sensors = Column(String, default="no")
     DoubleOwned = Column(String, default="no")
-
-    def insert_from_dict(self, item: dict):
-        """
-        Helper method to insert a dict object
-        :param item:
-        :return:
-        """
-        attribute_names = [attrib for attrib in dir(self) if attrib[0].isupper()]
-
-        new_item = {**self.__dict__, **item}
-
-        for name in attribute_names:
-            try:
-                self.__setattr__(name, new_item[name])
-            except KeyError:
-                pass
-
-    @classmethod
-    def property_names(cls) -> dict:
-        """
-        Helper method to return all class attribute names (Table Column Names) back in snake case to access as
-        properties in controller code.
-
-        :return: A list of snake_case property names matching those of the database column names.
-        """
-        # Get all the Column attributes
-        column_names = [name for name in dir(cls) if name[0].isupper()]
-
-        property_names = []
-
-        for name in column_names:
-            # Check to see if the name is an abbreviation i.e. ROT
-            is_all_upper = [1 if letter.isupper() else 0 for letter in name[1:]]
-
-            # If not an abbreviation
-            if min(is_all_upper) == 0:
-                # place a underscore in front of every capital, starting after the first letter.
-                letters = [f"_{letter}" if letter.isupper() else letter for letter in name[1:]]
-                # join the result and lowercase all letters to create snake case format.
-                property_names.append(name[0].lower() + "".join(letters).lower())
-            else:
-                # lower case the abbreviation.
-                property_names.append(name.lower())
-
-        return {column_name: property_name for column_name, property_name in zip(column_names, property_names)}
 
 
 class Unit(Core):
